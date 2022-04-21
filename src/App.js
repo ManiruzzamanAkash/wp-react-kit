db332f8e4f32a934a9db790092c027d67e99df95
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-04-21 23:53:55 +0600

    Add: Added a testing purpose Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
     return (
         <div>
             <h2 className='app-title'>Job Place App</h2>
             <hr />
+            <Dashboard />
         </div>
      );
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div className='dashboard'>
+            <div className="card">
+                <h3>Dashboard</h3>
+                <p>
+                    Edit Dashboard component at <code>src/components/Dashboard.jsx</code>
+                </p>
+            </div>
+        </div>
+     );
+}
+
+export default Dashboard;
