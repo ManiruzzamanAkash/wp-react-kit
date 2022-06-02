f36a32beec6ca9bf9642e64b163ddad97b58a96c
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-06-02 12:58:49 +0600

    add: Added eslint configuration and i18n package

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const defaults = require('@wordpress/scripts/config/webpack.config');
 
 module.exports = {
-  ...defaults,
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-  },
+    ...defaults,
 };
